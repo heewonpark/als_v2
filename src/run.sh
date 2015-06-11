51438b09cac7c8d96ae38905aca5413698e4832d
PARK ANTENNA <park.brainlab@gmail.com>
2015-06-11 14:30:23 +0900

    20150611 14:30

--- src/run.sh
+++ src/run.sh
@@ -4,10 +4,11 @@ NRNIV="../specials/x86_64/special -mpi"
 HOC_NAME="./main.hoc"
 
 NRNOPT=\
-" -c STOPTIME=50"\
+" -c STOPTIME=100"\
 " -c IS_SUPERCOMPUTER=0"
 
 MPIEXEC="mpiexec -n 4"
+#MPIEXEC=""
 
 EXEC="${MPIEXEC} ${NRNIV} ${NRNOPT} ${HOC_NAME}"
 
