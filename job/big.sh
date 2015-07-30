e9fd935330697de3d15491373c1c8297f18634a1
PARK KCOMPUTER <park.brainlab@gmail.com>
2015-07-30 21:21:53 +0900

    20150730

--- job/big.sh
+++ job/big.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #PJM --rsc-list "node=16"
-#PJM --rsc-list "elapse=1:00:00"
+#PJM --rsc-list "elapse=3:00:00"
 #PJM --rsc-list "rscgrp=small"
 #PJM --mpi "proc=128"
 #PJM -s
@@ -45,18 +45,18 @@ NRNIV="./special -mpi"
 HOC_NAME="./main.hoc"
 #NRNOPT=""
 NRNOPT=\
-" -c STOPTIME=100"\
+" -c STOPTIME=3600"\
 " -c IS_SUPERCOMPUTER=1"\
 " -c INTERVAL=1200"\
-" -c WEIGHT_200=0.200"\
+" -c WEIGHT_200=0.600"\
 " -c WEIGHT_300=0.008"\
 " -c WEIGHT_301=0.002"\
 " -c GABAA_GMAX_LTOL=5.0"\
 " -c GABAB_GMAX_LTOL=5.0"\
 " -c GABAA_GMAX_LTOP=0.1"\
-" -c GABAB_GMAX_LTOP=0.3"\
+" -c GABAB_GMAX_LTOP=0.01"\
 " -c GABAB_ON=1"\
-" -c GABAA_ON=0"
+" -c GABAA_ON=1"
 
 LPG="lpgparm -t 4MB -s 4MB -d 4MB -h 4MB -p 4MB"
 MPIEXEC="mpiexec -mca mpi_print_stats 1"
